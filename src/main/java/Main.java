9c9a3e9c4f25b90c9617fd5f4ba95e30b0cb0e8b
Kalp <kalpshah2003@gmail.com>
2023-03-06 09:18:28 -0500

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import mcunit.Assertions;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Testing");
+
+        Assertions.assertEquals(1 + 1, 3);
+    }
+}
